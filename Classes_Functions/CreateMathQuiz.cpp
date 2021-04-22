eefaf4fc7262a1cab56fd37aed7d5231400c355f
Mitul Patel <>
2021-04-22 14:50:19 +0000

    created "create your own math quiz" file and added it to the new classes and functions folder. added rudimentary code to CreateMathQuiz.cpp

---
+++ Classes_Functions/CreateMathQuiz.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cmath>
+#include <ctime>
+using namespace std;
+using std::pow;
+
+int main() {
+    /*
+    6 questions total
+    Of the 6 questions:
+        3 questions use random numbers
+        1 question uses pow
+        2 questions must use rounding - 1 that does round to whole number and 1 that does roundf to 2 decimal places
+    After they type in their answer to a question tell them if they are right or wrong.
+    Example:  You got it right!  It was 3                     or                  Sorry you got it wrong. The answer was 3
+    */
+
+
+    int true_false = 0;
+    //1 is true and 0 is false
+
+    //question 1
+    
+}
