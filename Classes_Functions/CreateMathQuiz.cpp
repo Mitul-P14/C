418170135a4a82176ee9c1ae4de656fe46701d17
Mitul-P14 <m5614p@gmail.com>
2021-04-23 18:15:07 -0400

    Update CreateMathQuiz.cpp
    completed questions 1-2 and tested them

--- Classes_Functions/CreateMathQuiz.cpp
+++ Classes_Functions/CreateMathQuiz.cpp
@@ -1,24 +1,69 @@
 #include <iostream>
 #include <cmath>
 #include <ctime>
+#include <string>
+#include <sstream>
 using namespace std;
 using std::pow;
+using std::string;
+
+void Correct ();
+void Wrong (int);
 
 int main() {
     /*
     6 questions total
     Of the 6 questions:
-        3 questions use random numbers
-        1 question uses pow
+        2 questions use random numbers
+         question uses pow
         2 questions must use rounding - 1 that does round to whole number and 1 that does roundf to 2 decimal places
     After they type in their answer to a question tell them if they are right or wrong.
     Example:  You got it right!  It was 3                     or                  Sorry you got it wrong. The answer was 3
     */
 
 
-    int true_false = 0;
-    //1 is true and 0 is false
+
+    double userAnswer = 0;
+    double correctAnswer = 0;
+
 
     //question 1
-    
+    int random = 1 + rand() % (20-1+1);
+    int rand2 = 1 + rand() % (20-1+1);
+
+    cout << "\n\nQuestion 1:\n" << random << "+" << rand2 << " equals what?\n";
+    cin >> userAnswer;
+
+    correctAnswer = random + rand2;
+
+    if (userAnswer == correctAnswer) {Correct();}
+    else {Wrong(correctAnswer);}
+
+
+
+    //question 2
+    cout << "\n\n(3*4)^2 equals what?\n";
+    cin >> userAnswer;
+
+    correctAnswer = pow(12, 2);
+
+    if (userAnswer == correctAnswer) {Correct();}
+    else {Wrong(correctAnswer);}
+
+
+
+    //question 3
+
+}
+
+
+
+void Correct()
+{
+    cout << "\nGreat Job! That was the correct answer!\n";
+}
+
+void Wrong(int right)
+{
+    cout << "\nSorry but that\'s the wrong answer\nThe correct answer was " << right << "\n";
 }
